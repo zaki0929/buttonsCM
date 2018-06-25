0fc5b6c169a9e8909d8f2f7f40aa733bdad3cf50
Ryo Okazaki <ryo.okazaki.0929@gmail.com>
2018-06-25 16:03:55 +0900

    Draw a button and move it obliquely

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,18 +1,39 @@
 #include "ofApp.h"
 
 //--------------------------------------------------------------
-void ofApp::setup(){
+void drawButton(int x, int y, int r){
+  // 円の解像度の指定
+  ofSetCircleResolution(64);
 
-}
+  // 本体の描画 
+  ofSetColor(200, 200, 200);
+  ofCircle(x, y, r);
+
+  // 穴の描画
+  ofSetColor(30, 30, 30);
+  ofCircle(x-(r/5), y-(r/5), r/8);
+  ofCircle(x+(r/5), y-(r/5), r/8);
+  ofCircle(x-(r/5), y+(r/5), r/8);
+  ofCircle(x+(r/5), y+(r/5), r/8); 
+} 
+
+//--------------------------------------------------------------
+void ofApp::setup(){
+  ofBackground(30, 30, 30);
+  bx = 0;
+  by = 0;
+  br = 100;
+} 
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+  bx++;
+  by++;
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+  drawButton(bx, by, br);
 }
 
 //--------------------------------------------------------------
@@ -23,7 +44,7 @@ void ofApp::keyPressed(int key){
 //--------------------------------------------------------------
 void ofApp::keyReleased(int key){
 
-}
+} 
 
 //--------------------------------------------------------------
 void ofApp::mouseMoved(int x, int y ){
